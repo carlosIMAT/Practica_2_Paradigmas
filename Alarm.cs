0ab97c4559cf006d4795e31683ca6395f73c0f25
carlosIMAT <112658387+carlosIMAT@users.noreply.github.com>
2024-10-03 12:15:31 +0200

    Funcionamiento de alarma

---
+++ Alarm.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice_2
+{
+    class Alarm
+    {
+        private bool isActive = false;
+        public Alarm() { }
+
+        public void ChangeState()
+        {
+            isActive = !isActive;
+        }
+
+        public bool IsActive()
+        {
+            return isActive;
+        }
+    }
+}
---
+++ AlarmController.cs
@@ -0,0 +1,60 @@
+﻿namespace Practice_2
+{
+    class AlarmController
+    {
+        private Alarm alarm;
+        private List<PoliceCar> policeCars;
+        private PoliceStation policeStation;
+
+        public AlarmController(Alarm alarm, PoliceStation policeStation)
+        {
+            this.alarm = alarm;
+            this.policeCars = new List<PoliceCar>();
+            this.policeStation = policeStation;
+        }
+
+        public void RegisterPoliceCar(PoliceCar policeCar)
+        {
+            policeCars.Add(policeCar);
+        }
+
+        public void ActivateAlarm(VehicleWithPlate vehicleWithPlate)
+        {
+            if (!alarm.IsActive())
+            {
+                alarm.ChangeState();
+                Console.WriteLine(policeStation.WriteMessage("The alarm has been activated."));
+
+                foreach (var policeCar in policeCars)
+                {
+                    if (policeCar.IsPatrolling())
+                    {
+                        policeCar.ChaseVehicle(vehicleWithPlate);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(policeStation.WriteMessage("The alarm is already active."));
+            }
+        }
+
+        public void DeactivateAlarm()
+        {
+            if (alarm.IsActive())
+            {
+                alarm.ChangeState();
+                Console.WriteLine(policeStation.WriteMessage("The alarm has been desactivated."));
+
+                foreach (var policeCar in policeCars)
+                {
+                    policeCar.StopChaseVehicle();
+                }
+            }
+            else
+            {
+                Console.WriteLine(policeStation.WriteMessage("The alarm is not active."));
+            }
+        }
+    }
+}
